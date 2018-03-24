804ae2c59af43768d93b02c9fae0d60f42c2f21f
ArturN <nizamutdin.art@yandex.ru>
2018-03-24 11:49:21 +0400

    Create separate BrowserSession

--- src/api-gateways/gibdd-api/gibdd.api.js
+++ src/api-gateways/gibdd-api/gibdd.api.js
@@ -1,44 +1,14 @@
-const puppeteer = require('puppeteer');
-
-const sleep = async delay => {
-    return new Promise(resolve => {
-        setTimeout(resolve, delay);
-    });
-};
-
-const waitForReload = async page => {
-    return new Promise(resolve => {
-        page.once('load', resolve);
-    });
-};
+const BrowserSession = require('./helpers/browser-session');
 
 class GibddApi {
     async sendRequest() {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
+        const browserSession = new BrowserSession();
 
-        console.log('Open the page...');
-        // see also https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pagegotourl-options
-        await page.goto('https://xn--90adear.xn--p1ai/request_main', {
-            waitUntil: 'networkidle0'
-        });
+        await browserSession.create();
 
-        console.log('Click checkbox "С информацией ознакомлен"...');
-        await page.click('.ln-content-holder form label.checkbox');
+        await browserSession.sendRequest(); // todo get user data from db and chatBot
 
-        console.log('Click button "Подать обращение"...');
-        await page.click('.ln-content-holder form .u-form__sbt');
-
-        console.log('Waiting for navigation...');
-        await waitForReload(page); // page.waitForNavigation not working if page reload happens
-
-        console.log('Make screenshot...');
-        await page.screenshot({ path: 'gibdd-start-newPage.png' });
-
-        // TODO capture Captcha element for screenshot https://gist.github.com/malyw/b4e8284e42fdaeceab9a67a9b0263743
-
-        console.log('Close the browser...');
-        await browser.close();
+        await browserSession.destroy();
     }
 }
 
---
+++ src/api-gateways/gibdd-api/helpers/browser-session.js
@@ -0,0 +1,69 @@
+const puppeteer = require('puppeteer');
+
+const sleep = async delay => {
+    return new Promise(resolve => {
+        setTimeout(resolve, delay);
+    });
+};
+
+const waitForReload = async page => {
+    return new Promise(resolve => {
+        page.once('load', resolve);
+    });
+};
+
+class BrowserSession {
+    constructor() {
+        this.browser = null;
+        this.page = null;
+    }
+
+    async create() {
+        this.browser = await puppeteer.launch();
+        this.page = await this.browser.newPage();
+    }
+
+    async sendRequest({
+        firstName,
+        lastName,
+        email,
+        region,
+        subdivision,
+        requestDescription
+    } = {}) {
+        await this._openStartGibddPage();
+
+        await this._acceptTermsAndConditions();
+
+        console.log('Make screenshot...');
+        await this.page.screenshot({ path: 'gibdd-start-newPage.png' });
+
+        // TODO capture Captcha element for screenshot https://gist.github.com/malyw/b4e8284e42fdaeceab9a67a9b0263743
+    }
+
+    async destroy() {
+        console.log('Close the browser...');
+        return await this.browser.close();
+    }
+
+    async _openStartGibddPage() {
+        console.log('Open the page...');
+        // see also https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pagegotourl-options
+        await this.page.goto('https://xn--90adear.xn--p1ai/request_main', {
+            waitUntil: 'networkidle0'
+        });
+    }
+
+    async _acceptTermsAndConditions() {
+        console.log('Click checkbox "С информацией ознакомлен"...');
+        await this.page.click('.ln-content-holder form label.checkbox');
+
+        console.log('Click button "Подать обращение"...');
+        await this.page.click('.ln-content-holder form .u-form__sbt');
+
+        console.log('Waiting for navigation...');
+        await waitForReload(this.page); // page.waitForNavigation not working if page reload happens
+    }
+}
+
+module.exports = BrowserSession;
